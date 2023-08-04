af5a1882decd135d0d3b2baed728f2473186b2e4
Jay Lim <jlimoman@gmail.com>
2023-08-04 17:24:03 -0600

    refactor <ScoreKeeper /> to use pre-defined event handlers

--- src/components/ScoreKeeper.js
+++ src/components/ScoreKeeper.js
@@ -7,24 +7,30 @@ const ScoreKeeper = () => {
   let [ p1Score, setP1Score ] = useState(0);
   let [ p2Score, setP2Score ] = useState(0);
 
+  let winningScore = 5;
 
-  // const p1Handler = () => {
-  //   setP1Score(p1Score++);
-  // }
+  const p1Handler = () => {
+    if (p1Score !== winningScore) {
+      setP1Score(p1Score++);
+    }
+    
+  }
 
-  // const p2Handler = () => {
-  //   setP2Score(p2Score++);
-  // }
+  const p2Handler = () => {
+
+    setP2Score(p2Score++);
+  }
 
   const resetHandler = () => {
     alert('reset clicked!')
   }
 
+
   return (
     <>
       <h1><span id='p1Score'>{p1Score}</span> to <span id='p2Score'>{p2Score}</span></h1>
-      <button id='p1Button' onClick={() => setP1Score(p1Score++)}>+1 Player One</button>
-      <button id='p2Button' onClick={() => setP2Score(p2Score++)}>+1 Player Two</button>
+      <button id='p1Button' onClick={p1Handler}>+1 Player One</button>
+      <button id='p2Button' onClick={p2Handler}>+1 Player Two</button>
       <button id='reset' onClick={resetHandler}>Reset</button>    
     </>
   )
