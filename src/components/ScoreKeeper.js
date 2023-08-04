fd66de32ea6e3309e0fe20d68a9b453b8fb3cf13
Jay Lim <jlimoman@gmail.com>
2023-08-04 17:19:48 -0600

    modify README and some logic in <ScoreKeeper />

--- src/components/ScoreKeeper.js
+++ src/components/ScoreKeeper.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ScoreKeeper.css';
 
 const ScoreKeeper = () => {
+
+  // if state is declared using const, wil throw 'Assignment to constant variable' error
+  let [ p1Score, setP1Score ] = useState(0);
+  let [ p2Score, setP2Score ] = useState(0);
+
+
+  // const p1Handler = () => {
+  //   setP1Score(p1Score++);
+  // }
+
+  // const p2Handler = () => {
+  //   setP2Score(p2Score++);
+  // }
+
+  const resetHandler = () => {
+    alert('reset clicked!')
+  }
+
   return (
-    <h1>ScoreKeeper</h1>
+    <>
+      <h1><span id='p1Score'>{p1Score}</span> to <span id='p2Score'>{p2Score}</span></h1>
+      <button id='p1Button' onClick={() => setP1Score(p1Score++)}>+1 Player One</button>
+      <button id='p2Button' onClick={() => setP2Score(p2Score++)}>+1 Player Two</button>
+      <button id='reset' onClick={resetHandler}>Reset</button>    
+    </>
   )
 }
 
