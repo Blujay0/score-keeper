942a7c552611323e856dcb8c727246385b911a46
Jay Lim <jlimoman@gmail.com>
2023-08-04 19:00:45 -0600

    working score keeper with no styling

--- src/components/ScoreKeeper.js
+++ src/components/ScoreKeeper.js
@@ -9,8 +9,8 @@ const ScoreKeeper = () => {
   let [ p2Score, setP2Score ] = useState(0);
   let [ isGameOver, setIsGameOver ] = useState(false);
   let [ winningScore, setWinningScore ] = useState(null);
-  let [ p1WinStatus, setP1WinStatus ] = useState(false);
-  let [ p2WinStatus, setP2WinStatus ] = useState(false);
+  // let [ p1WinStatus, setP1WinStatus ] = useState(false);
+  // let [ p2WinStatus, setP2WinStatus ] = useState(false);
 
   const winnerStyle = { color: "green" };
   const loserStyle = { color: "red" };
@@ -26,7 +26,7 @@ const ScoreKeeper = () => {
       // if score is winning score, game over
       if (p1Score === winningScore) {
         // alert('Player 1 wins!');
-        setP1WinStatus(true);
+        // setP1WinStatus(true);
         setIsGameOver(true);
       }
     }
@@ -40,7 +40,7 @@ const ScoreKeeper = () => {
       // if score is winning score, game over
       if (p2Score === winningScore) {
         // alert('Player 2 wins!');
-        setP2WinStatus(true);
+        // setP2WinStatus(true);
         setIsGameOver(true);
       }
     }
@@ -48,6 +48,8 @@ const ScoreKeeper = () => {
 
   const resetHandler = (e) => {
     setIsGameOver(false);
+    // setP1WinStatus(false);
+    // setP2WinStatus(false);
     setP1Score(0);
     setP2Score(0);
   }
@@ -58,14 +60,16 @@ const ScoreKeeper = () => {
 
     // e.target.value holds the value of the <select>
     // use parseInt b/c e.target.value is a string
+
     setWinningScore(parseInt(e.target.value));
     resetHandler();
   }
 
   return (
     <>
-      <h1><span id='p1Score' style={p1WinStatus ? { winnerStyle } : { loserStyle }}>{p1Score}</span> to <span id='p2Score' style={p2WinStatus ? { winnerStyle } : { loserStyle }}>{p2Score}</span></h1>
-
+      {/* <h1><span id='p1Score' style={ p1WinStatus ? winnerStyle : loserStyle }>{p1Score}</span> to <span id='p2Score' style={{p2WinStatus} ? { winnerStyle } : { loserStyle }}>{p2Score}</span></h1> */}
+      <h1><span id='p1Score'>{p1Score}</span> to <span id='p2Score'>{p2Score}</span></h1>
+      
       <select name='' id='playto' onChange={winningScoreSelect}>
         <option value='3'>3</option>
         <option value='4'>4</option>
@@ -80,7 +84,7 @@ const ScoreKeeper = () => {
 
       <button id='p1Button' onClick={p1Handler}>+1 Player One</button>
       <button id='p2Button' onClick={p2Handler}>+1 Player Two</button>
-      <button id='reset' onClick={resetHandler}>Reset</button>    
+      <button id='reset' onClick={resetHandler}>Reset</button>  
     </>
   )
 }
